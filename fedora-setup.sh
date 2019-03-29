e4889e13e25d591a3ceee7ebdb4f96286cb69116
Ismaïl Lachheb <ismaellachheb@gmail.com>
2019-03-29 10:33:38 +0100

    removed sudo

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/bash
 cd ~
 # Update cache for package installs
-sudo dnf makecache
+dnf makecache
 #Update
-sudo dnf upgrade
+dnf upgrade
 # install flahub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # install winepak repository
 flatpak remote-add --if-not-exists winepak https://dl.winepak.org/repo/winepak.flatpakrepo
 # Install Rpmfusion repo
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 #Swap gnome with plasma
-sudo dnf swap @gnome-desktop @kde-desktop
+dnf remove gdm gnome-shell gtk3
+dnf install @kde-desktop sddm
 # Install Rstudio
-sudo dnf install https://download1.rstudio.org/rstudio-1.1.463-x86_64.rpm -y
+dnf install https://download1.rstudio.org/rstudio-1.1.463-x86_64.rpm -y
 # grab all packages to install
-sudo dnf install $(cat ~/fedora.packages) -y
+dnf install $(cat ~/fedora.packages) -y
 
 # install python packages
 pip3 --user install $(cat ~/python.packages)
@@ -26,11 +27,11 @@ pip --user install $(cat ~/python2.packages)
 flatpak install flathub $(cat ~/flathub.packages) -y
 
 # download anaconda
-wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
-bash Anaconda3-5.3.0-Linux-x86_64.sh
+#wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+#bash Anaconda3-5.3.0-Linux-x86_64.sh
 
 # web development (Mariadb)
 
-sudo /usr/bin/mysql_secure_installation
+/usr/bin/mysql_secure_installation
 
 # R packages
