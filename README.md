717480d06f52fe00ece0049caa07bc26bd8ca37d
Ismaël Lachheb <isma@localhost.localdomain>
2019-05-06 17:36:49 +0200

    working version

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # fedora-setup
 my fedora setup
 
-# utilisation 
+# utilisation
 
-run 
-
-	wget https://raw.githubusercontent.com/lachhebo/fedora-setup/master/fedora-setup.sh && sudo chmod +x fedora-setup.sh && ./fedora-setup.sh
 
 # warning
 check the version of Rstudio
--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+dir=$(pwd)
+cd $dir
 
 # install flahub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -7,19 +9,19 @@ flatpak remote-add --if-not-exists winepak https://dl.winepak.org/repo/winepak.f
 # Install Rpmfusion repo
 #sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 # Update cache for package installs
-sudo dnf makecache
-sudo dnf upgrade
+dnf makecache
+dnf upgrade
 # Install Rstudio
 #sudo dnf install https://download1.rstudio.org/rstudio-1.1.463-x86_64.rpm -y
 # grab all packages to install
-sudo dnf install $(cat ~/fedora.packages) -y
+dnf install $(cat fedora.packages) -y
 
 # install python packages
-pip3 --user install $(cat ~/python.packages)
+pip3 --user install $(cat python.packages)
 #pip --user install $(cat ~/python2.packages)
 
 # grab all flatpak to install
-flatpak install flathub $(cat ~/flathub.packages) -y
+flatpak install flathub $(cat flathub.packages) -y
 
 # download anaconda
 #wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
