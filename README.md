511b30646db0b3dc0e991a2ea164ee9137bd3f16
ismael lachheb <ismael.lachheb@protonmail.com>
2020-03-29 15:01:07 +0200

    cleaning

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # fedora-setup
 
-those scripts allow to setup a fedora workstation with the desktop environment of your choice 
+this script allow to setup a fedora workstation with the desktop environment of your choice 
 
-## utilisation
+## Usage
 
 
 ```sh  
 	cd fedora-setup
 	chmod +x install_setup.sh
-	sudo ./install_setup.sh [DESKTOP_ENV]
+	sudo ./install_setup.sh [DESKTOP_ENV] [OPTIONS]
 ```
+
+Instaled packages are parametrable with configurations file : 
+
+- rpm : rpm packages installed with dnf
+- flathub : flatpak packages installed from flathub repository
+- python : pip packages installed 
+
+### DESKTOP_ENV
 
 You can choose the desktop envionment of your choice with the DESKTOP_ENV parameter : 
 - GNOME
@@ -25,22 +33,13 @@ You can change the desktop environment by running the script with an other DESKT
 	sudo ./install_setup.sh PLASMA
 ```
 
-Instaled packages are parametrable with the configuration file : 
-- fedora.packages : rpm packages installed for any desktop env
-- gnome.packages : rpm packages installed for the gnome desktop
-- plasma.packages : rpm packages installed for the plasma desktop
 
-- flathub.packages : flathub packages installed for any desktop env
-- gnome_flatpak.packages : flathub packages installed for the gnome desktop
-- plasma_flatpak.packages : flathub packages installed for the plasma desktop
+### OPTIONS
 
-- python.packages : pip packages installed 
 
-## Todo 
-
-- [ ] automatically install R packages
-- [ ] parameter syncthing
-- [ ] make the used shell as a parameter
-
+```sh  
+	--nvidia : install nvidia propietary driver
+	--zsh : replace current shell with zsh and install oh-my-zsh
+```
 
 
--- install_setup.sh
+++ install_setup.sh
@@ -6,54 +6,55 @@ PLASMA_DESKTOP="PLASMA"
 dir=$(pwd)
 cd $dir
 
+
 # RESOURCES
 
 # install flahub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
 # install RPMFusion repo
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y -q
-
 # update fedora
 sudo dnf upgrade -y -q
 
 # SHELL
 
 # uses zsh instead of bash 
-chsh -s $(which zsh)
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -q
-sh install.sh --unattended
-cat zshrc.aliases >> ~/.zshrc
+install_zsh_func(){
+	chsh -s $(which zsh)
+	wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -q
+	sh install.sh --unattended
+	cat zshrc.aliases >> ~/.zshrc
+}
+
 
 
 # BASIC PACKAGES
 
 # install basic packages 
-sudo dnf install $(cat fedora.packages) -y -q
+sudo dnf install $(cat packages/base/rpm) -y -q
 # install python packages
-pip install --user -q $(cat python.packages)
+pip install --user -q $(cat packages/base/python)
 # grab all flatpak to install
-flatpak install flathub $(cat flathub.packages) -y --noninteractive
+flatpak install flathub $(cat packages/base/flathub) -y --noninteractive
 
 # DESKTOP
 
 if [ $1 = $GNOME_DESKTOP ]
 then
-	sudo dnf remove $(cat plasma.packages) -y -q 
-	#flatpak uninstall $(cat plasma_flatpak.packages) -y --noninteractive
+	sudo dnf remove $(cat packages/plasma/rpm) -y -q 
 
-	sudo dnf install $(cat gnome.packages) -y -q
+	sudo dnf install $(cat packages/gnome/rpm) -y -q
 	sudo systemctl enable gdm
-	flatpak install flathub $(cat gnome_flatpak.packages) -y --noninteractive
+	flatpak install flathub $(cat packages/gnome/flathub) -y --noninteractive
 	
 elif [ $1 = $PLASMA_DESKTOP ]
 then
-	sudo dnf remove $(cat gnome.packages) -y  -q
+	sudo dnf remove $(cat packages/gnome/rpm) -y  -q
 	sudo dnf remove gnome-* gdm tracker-miners gtk3 -y -q
-	#flatpak uninstall $(cat gnome_flatpak.packages) -y  --noninteractive
 
-	sudo dnf install $(cat plasma.packages) -y -q
+	sudo dnf install $(cat packages/plasma/rpm) -y -q
 	sudo systemctl enable sddm.service
-	flatpak install flathub $(cat plasma_flatpak.packages) -y --noninteractive
+	flatpak install flathub $(cat packages/plasma/flathub) -y --noninteractive
 	 
 fi
 
@@ -65,6 +66,18 @@ sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.micros
 sudo dnf check-update -q
 sudo dnf install code -y -q
 
+# install nvidia-driver
 
+
+# Parse arguments
+	while [ $# -gt 0 ]; do
+		case $1 in
+			--nvidia) sudo dnf install akmod-nvidia -y -q;;
+			--zsh) install_zsh_func;;
+		esac
+		shift
+	done
+
+	
 
 
