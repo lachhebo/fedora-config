78a8f295dda5f72e47a98fd0801f0c551ec2f414
ismaël <ismael.lachheb@protonmail.com>
2020-04-01 12:55:08 +0200

    improve script after a change from kde to gnome

--- install_setup.sh
+++ install_setup.sh
@@ -7,17 +7,6 @@ dir=$(pwd)
 cd $dir
 
 
-# RESOURCES
-
-# install flahub repository
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
-# install RPMFusion repo
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y -q
-# update fedora
-sudo dnf upgrade -y -q
-
-# SHELL
-
 # uses zsh instead of bash 
 install_zsh_func(){
 	chsh -s $(which zsh)
@@ -26,57 +15,74 @@ install_zsh_func(){
 	cat zshrc.aliases >> ~/.zshrc
 }
 
-
-
-# BASIC PACKAGES
-
-# install basic packages 
-sudo dnf install $(cat packages/base/rpm) -y -q
-# install python packages
-pip install --user -q $(cat packages/base/python)
-# grab all flatpak to install
-flatpak install flathub $(cat packages/base/flathub) -y --noninteractive
-
-# DESKTOP
-
-if [ $1 = $GNOME_DESKTOP ]
-then
-	sudo dnf remove $(cat packages/plasma/rpm) -y -q 
-
-	sudo dnf install $(cat packages/gnome/rpm) -y -q
-	sudo systemctl enable gdm
-	flatpak install flathub $(cat packages/gnome/flathub) -y --noninteractive
-	
-elif [ $1 = $PLASMA_DESKTOP ]
-then
-	sudo dnf remove $(cat packages/gnome/rpm) -y  -q
-	sudo dnf remove gnome-* gdm tracker-miners gtk3 -y -q
-
-	sudo dnf install $(cat packages/plasma/rpm) -y -q
-	sudo systemctl enable sddm.service
-	flatpak install flathub $(cat packages/plasma/flathub) -y --noninteractive
-	 
-fi
-
-# OTHERs
-
 # install visual studio code
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update -q
-sudo dnf install code -y -q
 
-# install nvidia-driver
+install_visual_studio_code(){
+	rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+	sudo dnf check-update -q
+	sudo dnf install code -y -q
+}
+
+
+main(){
+
+	# RESOURCES
+
+	# install flahub repository
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+	# install RPMFusion repo
+	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y -q
+	# update fedora
+	sudo dnf upgrade -y -q
+
+
+	# DESKTOP
+
+	if [ $1 = $GNOME_DESKTOP ]
+	then
+		sudo dnf remove $(cat packages/plasma/rpm) -y -q 
+		sudo dnf remove kde* dnfdragora
+		rm -r ~/.config/gtk* ## remove modifications applied by plasma
+		rm ~/.config/.gtk*
+
+		sudo dnf install $(cat packages/gnome/rpm) -y -q
+		sudo systemctl enable gdm
+		flatpak install flathub $(cat packages/gnome/flathub) -y --noninteractive
+		
+	elif [ $1 = $PLASMA_DESKTOP ]
+	then
+		sudo dnf remove $(cat packages/gnome/rpm) -y  -q
+		sudo dnf remove gnome-* gdm tracker-miners gtk3 -y -q
+
+		sudo dnf install $(cat packages/plasma/rpm) -y -q
+		sudo systemctl enable sddm.service
+		flatpak install flathub $(cat packages/plasma/flathub) -y --noninteractive
+	fi
+
+
+	# install basic packages 
+	sudo dnf install $(cat packages/base/rpm) -y -q
+	# install python packages
+	pip install --user -q $(cat packages/base/python)
+	# grab all flatpak to install
+	flatpak install flathub $(cat packages/base/flathub) -y --noninteractive
+}
+
 
 
 # Parse arguments
-	while [ $# -gt 0 ]; do
-		case $1 in
-			--nvidia) sudo dnf install akmod-nvidia -y -q;;
-			--zsh) install_zsh_func;;
-		esac
-		shift
-	done
+while [ $# -gt 0 ]; do
+	case $1 in
+		--nvidia) sudo dnf install akmod-nvidia -y -q;;
+		--zsh) install_zsh_func;;
+		--vscode) install_visual_studio_code;;
+	esac
+	shift
+done
+
+
+main()
 
 	
 
